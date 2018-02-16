0bd8588dd4d1faa091d8b53f8fa537d5f22a9910
crystalDf <chendong333@gmail.com>
2018-02-16 13:24:01 +0800

    Chapter 06 SharedPreferencesTest

--- app/src/main/java/com/star/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/star/sharedpreferencestest/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,33 +18,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSaveDataButton = (Button) findViewById(R.id.save_data);
-        mSaveDataButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getSharedPreferences(FILE_NAME, MODE_PRIVATE)
-                        .edit()
-                        .putString("name", "Tom")
-                        .putInt("age", 28)
-                        .putBoolean("married", false)
-                        .apply();
-            }
-        });
+        mSaveDataButton = findViewById(R.id.save_data);
+        mSaveDataButton.setOnClickListener(v -> getSharedPreferences(FILE_NAME, MODE_PRIVATE)
+                .edit()
+                .putString("name", "Tom")
+                .putInt("age", 28)
+                .putBoolean("married", false)
+                .apply());
 
-        mLoadDataButton = (Button) findViewById(R.id.load_data);
-        mLoadDataButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sharedPreferences = getSharedPreferences(FILE_NAME, MODE_PRIVATE);
+        mLoadDataButton = findViewById(R.id.load_data);
+        mLoadDataButton.setOnClickListener(v -> {
+            SharedPreferences sharedPreferences = getSharedPreferences(FILE_NAME, MODE_PRIVATE);
 
-                String name = sharedPreferences.getString("name", "");
-                int age = sharedPreferences.getInt("age", 0);
-                boolean married = sharedPreferences.getBoolean("married", false);
+            String name = sharedPreferences.getString("name", "");
+            int age = sharedPreferences.getInt("age", 0);
+            boolean married = sharedPreferences.getBoolean("married", false);
 
-                Log.d("MainActivity", "name is " + name);
-                Log.d("MainActivity", "age is " + age);
-                Log.d("MainActivity", "married is " + married);
-            }
+            Log.d("MainActivity", "name is " + name);
+            Log.d("MainActivity", "age is " + age);
+            Log.d("MainActivity", "married is " + married);
         });
     }
 }
